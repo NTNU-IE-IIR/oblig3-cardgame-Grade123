f6a9640e859fea867f4f2dd22f2ae773c5409ea6
Grade123 <stianoyejenssen@gmail.com>
2024-03-01 12:29:57 +0100

    Added negative test

--- src/test/java/DeckOfCardsTest.java
+++ src/test/java/DeckOfCardsTest.java
@@ -1,4 +1,5 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +11,9 @@ import edu.stud.stianoj.cardgame.DeckOfCards;
  * positive test cases:
  * - test that you can construct a deck of cards with 52 cards
  * - test that you can get n random cards from the deck
+ * 
+ * negative test cases:
+ * - test that you cannot get more cards than the deck contains
  */
 public class DeckOfCardsTest {
     @Test
@@ -18,10 +22,17 @@ public class DeckOfCardsTest {
         assertEquals(52, deck.getNumberOfCards());
     }
 
-
     @Test
     public void testDealHand() {
         DeckOfCards deck = new DeckOfCards();
         assertEquals(5, deck.dealHand(5).size());
+    }
+
+    @Test
+    public void testDealHandNegative() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            DeckOfCards deck = new DeckOfCards();
+            deck.dealHand(53);
+        });
     }
 }
